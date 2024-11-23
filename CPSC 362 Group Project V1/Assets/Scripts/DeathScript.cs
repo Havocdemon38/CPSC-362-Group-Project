627cfbfb9019706d3f03f9128089f20b01fcdbca
joebernal <109630061+joebernal@users.noreply.github.com>
2024-11-23 15:45:54 -0800

    Fix Whitespace for DeathScript.cs

--- CPSC 362 Group Project V1/Assets/Scripts/DeathScript.cs
+++ CPSC 362 Group Project V1/Assets/Scripts/DeathScript.cs
@@ -9,13 +9,11 @@ public class DeathScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnCollisionEnter2D(Collision2D other)
